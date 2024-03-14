0369a1a161d59b600055767ce1b911501210fc2a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-14 13:24:59 +0900

    update tailwind

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,8 +2,8 @@
   <main class="min-h-[100vh]">
   <div class="max-w-[1200px] container mx-auto pt-[100px] p-5">
     <h2 class="text-3xl">ブログ一覧ページ</h2>
-    <div class="flex flex-col sm:flex-row gap-5 mt-10">
-      <NuxtLink v-for="post in data" :to="`/posts/${post.id}`" class="w-full sm:w-1/3 shadow rounded hover:opacity-80">
+    <div class="flex gap-[3.3%] mt-10 flex-col sm:flex-row sm:flex-wrap">
+      <NuxtLink v-for="post in data" :to="`/posts/${post.id}`" class="w-full sm:w-[30%] shadow rounded hover:opacity-80 mb-10">
           <img v-if="post.thumbnail_url" :src="post.thumbnail_url" :alt="post.title.rendered">
           <div class="p-4">
             <p class="text-xl">{{ post.title.rendered }}</p>
