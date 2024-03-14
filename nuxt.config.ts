ff3e2896f8cc0a135becf562eace5e4da2049afe
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-14 17:03:47 +0900

    add .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
